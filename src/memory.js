6d59f471a8675f4f1dd794cb27f79acb871f904e
Gerard Guasch <haldawe@hotmail.com>
2022-11-19 19:02:28 +0100

    finished memory.js

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    for (let i = this.cards.length; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * i);
+      [this.cards[randomIndex], this.cards[i - 1]] = [
+        this.cards[i - 1],
+        this.cards[randomIndex],
+      ];
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2) {
+      this.pairsGuessed++
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === this.cards.length / 2
   }
 }
